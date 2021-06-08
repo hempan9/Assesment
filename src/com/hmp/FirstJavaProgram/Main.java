562b67556c146b793fcc83999dea7be304b59eb8
hempan9 <hemantapant9@gmail.com>
2021-06-08 12:07:11 -0500

    files updated

---
+++ src/com/hmp/FirstJavaProgram/Main.java
@@ -0,0 +1,8 @@
+package com.hmp.FirstJavaProgram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java");
+    }
+}
---
+++ src/com/hmp/localvariables/LocalVariables.java
@@ -0,0 +1,15 @@
+package com.hmp.localvariables;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+        int age=20;
+        float loanAmount = 200.78f;
+        double loanTaken = 18977.5d;
+        boolean isLoanPaid = true;
+        System.out.println("Integer variable: "+age);
+        System.out.println("Floating point variable: "+loanAmount);
+        System.out.println("Double type variable value: "+loanTaken);
+        System.out.println("Boolean type variable value: "+isLoanPaid);
+
+    }
+}
