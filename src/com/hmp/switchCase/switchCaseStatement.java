d0429ba06a9ff91dfdbadc1977c779f5ee23377f
hempan9 <hemantapant9@gmail.com>
2021-06-10 22:05:40 -0500

    assignment files created and updated

--- src/com/hmp/switchCase/switchCaseStatement.java
+++ src/com/hmp/switchCase/switchCaseStatement.java
@@ -5,15 +5,27 @@ package com.hmp.switchCase;
 
 public class switchCaseStatement {
     public static void main(String[] args) {
-        char grade = 'B';
+        char grade = 'A';
         int marks;
         //incomplete
         switch (grade) {
             case 'A':
-                System.out.println("Grade A");
+                System.out.println("Range is 80-100");
                 break;
             case 'B':
-                System.out.println("Grade B");
+                System.out.println("Range is 73-79");
+                break;
+            case 'C':
+                System.out.println("Range is 65-72");
+                break;
+            case 'D':
+                System.out.println("Range is 55-64");
+                break;
+            case 'E':
+                System.out.println("Range is 0-55");
+                break;
+            default :
+                System.out.println("Garde does not exist");
                 break;
         }
     }
