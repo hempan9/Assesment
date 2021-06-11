a7cae118190aab9d917a4dddda6a6bec29540b0b
hempan9 <hemantapant9@gmail.com>
2021-06-10 23:53:17 -0500

    assignment files created and updated

--- src/com/hmp/codingStandards/Class1.java
+++ src/com/hmp/codingStandards/Class1.java
@@ -4,40 +4,37 @@
 package com.hmp.codingStandards;
 
 public class Class1 {
-    static int i=22;
-    static int j=14000;
-   static boolean k;
-   static boolean l;
+    static int i;
+    static int j;
+    static boolean k;
+    static boolean l;
 
     public static void main(String[] args) {
-        i=22;
-        j=14000;
+        i = 22;
+        j = 14000;
         Class1 class1 = new Class1();
-        k= class1.Method1(i);
-        l= class1.Method2(j);
-        if(k==true && l ==true){
+        k = class1.Method1(i);
+        l = class1.Method2(j);
+        if (k == true && l == true) {
             System.out.println("Is a new employee");
-        }
-        else
-        {
+        } else {
             System.out.println("Is not a new employee");
         }
     }
-    public boolean Method1(int i){
-        if(i>20 && i<30){
+
+    public boolean Method1(int i) {
+        if (i > 20 && i < 30) {
             return true;
-        }
-        else {
+        } else {
             return false;
 
         }
     }
-    public boolean Method2(int j){
-        if(j>=14000 && j<20000){
+
+    public boolean Method2(int j) {
+        if (j >= 14000 && j < 20000) {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
